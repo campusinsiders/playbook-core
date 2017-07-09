f4d713e266e0d3946ec496950d127c4a1b3e7142
Christian Chung <christian.m.chung@gmail.com>
2017-07-08 20:40:45 -0400

    class(attribute*): Remove type hints incompatible with interface

--- src/classes/ui/class-string-attribute.php
+++ src/classes/ui/class-string-attribute.php
@@ -34,7 +34,7 @@ class String_Attribute extends Base_Attribute implements Attribute {
 	 * @param null|callable $setter An optional callable setter, passed the desired value.
 	 * @param null|callable $getter An optional callable getter, passed the current value.
 	 */
-	public function __construct( string $name, string $value, callable $setter = null, callable $getter = null ) {
+	public function __construct( string $name, $value, callable $setter = null, callable $getter = null ) {
 		if ( $this->is_valid( $value ) ) {
 			$this->type = 'string';
 			parent::__construct( $name, $value, $setter, $getter );
